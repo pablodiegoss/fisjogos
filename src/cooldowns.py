3834d918881de79015e36f0aa85d4d7d4cb24fa9
Pablo Diego <pablodiegoss@hotmail.com>
2020-11-16 20:18:01 -0300

    Create Cooldown manager

---
+++ src/cooldowns.py
@@ -0,0 +1,22 @@
+from .utils import SingletonMeta
+from time import time
+from enum import Enum
+
+class TimedEvent(Enum):
+    WIND_CHANGE = 20
+    SHOOT = 1
+
+
+class Cooldown(metaclass=SingletonMeta):
+    time_table = {}
+
+    def __init__(self):
+        for event in TimedEvent:
+            self.time_table[event] = time() + event.value
+
+    def activate(self, timed_event):
+        if self.time_table[timed_event] < time():
+            self.time_table[timed_event] = time() + timed_event.value
+
+    def check(self, timed_event):
+        return self.time_table[timed_event] < time()
